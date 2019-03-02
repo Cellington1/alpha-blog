0aeab548b00c66cff92d032f293b7f631ba3459d
Cellington1 <cody.ellington@radiantsolutions.com>
2019-03-02 10:33:06 -0500

    add partials and destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-	root 'welcome#home'
-	get 'about', to: 'welcome#about'
-	get 'login', to: 'welcome#login'
+	root 'pages#home'
+
+	get 'about', to: 'pages#about'
+
+	get 'login', to: 'pages#login'
 
   resources :articles
 end
