597f6c519399c22f9370d9d5519514dc2b9b0f7a
Cellington1 <cody.ellington@radiantsolutions.com>
2019-03-02 10:39:10 -0500

    Extracted redundancy and prep for prod

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
 	def index
 		@articles = Article.all
 	end
@@ -8,7 +10,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	def create
@@ -27,7 +28,6 @@ class ArticlesController < ApplicationController
    
   def update
   	#render plain: params[:article].inspect
-		@article = Article.find(params[:id])
 		# @article.save
 		if @article.update(article_params)
 			# do something
@@ -40,11 +40,9 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article = Article.find(params[:id])
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
 
 		flash[:notice] = "Article deleted"
@@ -55,5 +53,10 @@ class ArticlesController < ApplicationController
   def article_params
   	params.require(:article).permit(:title, :description)
   end	
+  
+  private
+  def set_article
+  	@article = Article.find(params[:id])
+  end
 
 end
