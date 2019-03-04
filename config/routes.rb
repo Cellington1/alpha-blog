5fd6dc3807027fa195dc5ca6540761fed7c3d402
Cellington1 <cody.ellington@radiantsolutions.com>
2019-03-04 14:19:05 -0500

    added login form

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
 
 	root 'pages#home'
 
-	get 'about', to: 'pages#about'
-
-	get 'login', to: 'pages#login'
-
   resources :articles
 
   get 'signup',to: 'users#new'
   resources :users, except: [:new]
+
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+
 end
